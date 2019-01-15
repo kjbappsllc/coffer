6317388cc2d99af9c49a775a93d1381cc639a128
Keyshawn Butts <kbutts314@gmail.com>
2019-01-15 18:41:24 -0500

    added mac script for port forwarding 80 traffic to 8080

--- src/interfaces/cli/utils/index.js
+++ src/interfaces/cli/utils/index.js
@@ -1,9 +1,7 @@
-export const getScriptExt = ({ platform }) => platform === 'win32' ? 'bat' : 'sh'
-
 export const getHostsPath = ({ platform, path }) => {
-    const windowsPath = 'C:/Windows/System32/drivers/etc/hosts'
-    const unixPath = '/etc/hosts'
-    return platform === 'win32' ? path.relative('.', windowsPath) : unixPath
+    return platform === 'win32'
+        ? path.relative('.', 'C:/Windows/System32/drivers/etc/hosts')
+        : '/etc/hosts'
 }
 
 export const getFileOwnershipScript = ({ platform, hostsPath }) => {
@@ -15,17 +13,16 @@ export const getFileOwnershipScript = ({ platform, hostsPath }) => {
 export const getPermissionsScriptFn = ({ platform, hostsPath }) => ({
     username
 }) => {
-    //TODO get mac script working
     return platform === 'win32'
         ? `icacls ${hostsPath} /grant:r ${username.split('\\')[1].trim()}:(R,W) && icacls ${hostsPath} /reset`
         : 'echo skipping set permissions for MACOS environment'
 }
 
 export const getPortProxyScript = ({ platform }) => {
-    //TODO get mac script working
     const windowsScript = 'netsh interface portproxy add v4tov4 listenport=80 listenaddress=127.3.3.3 connectport=8080 connectaddress= 127.0.0.1'
-    const windowsScriptAdminRun = `powershell.exe -Command "Start-Process cmd -ArgumentList \'/c ${windowsScript}\' -Verb RunAs"`
-    return platform === 'win32' ? windowsScriptAdminRun : ''
+    return platform === 'win32'
+        ? `powershell.exe -Command "Start-Process cmd -ArgumentList \'/c ${windowsScript}\' -Verb RunAs"`
+        : 'echo "rdr pass inet proto tcp from any to 127.3.3.3 port 80 -> 127.0.0.1 port 8080 | sudo pfctl -ef -"'
 }
 
 export const getCliColors = ({ chalk }) => ({
