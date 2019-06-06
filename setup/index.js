fce22fed7d688946c0cf315c3e6219bf568ed9ad
Keyshawn J. Butts <keyshawn_j_butts@homedepot.com>
2019-06-06 14:21:01 -0400

    Added some scripts on the mac side to download docker

--- setup/index.js
+++
@@ -1,7 +0,0 @@
-import { setupHostsConfig } from './hosts-file'
-import { isCofferSetup } from './pre-check'
-
-export {
-    setupHostsConfig,
-    isCofferSetup
-}
--- src/macos/install.sh
+++ src/macos/install.sh
@@ -1,3 +1,64 @@
 #!/usr/bin/env bash
 
-echo "Hello World"
+##########
+# CHECK ENVIRONMENT
+#########
+
+text() {
+   echo " ====> ${1}"
+}
+
+command -v brew  > /dev/null 2>&1
+
+if [[ $? != 0 ]]; then
+    echo ""
+    text "Brew not installed"
+    text "Installing Brew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    text "Brew is installed"
+fi
+
+brew cask list | grep docker > /dev/null 2>&1
+
+if [[ $? != 0 ]]; then
+    echo ""
+    text "Docker is not installed"
+    text "Installing Docker"
+    brew cask install docker
+else
+    text "Docker is installed"
+fi
+
+command -v docker > /dev/null 2>&1
+
+if [[ $? != 0 ]]; then
+    text "Docker tools are not installed"
+    open -a "Docker"
+else
+    text "Docker tools are installed"
+fi
+
+text "Checking docker installation"
+docker run hello-world > /dev/null 2>&1
+
+if [[ $? != 0 ]]; then
+    text "Docker was not installed correctly"
+    exit 1
+else
+    text "Docker installed succcessfully"
+    docker ps -a | awk '{ print $1,$2 }' | grep hello-world | awk '{print $1 }' | xargs -I {} docker rm {} > /dev/null 2>&1
+    docker image rm hello-world > /dev/null 2>&1
+fi
+
+text "Installing necessary images"
+
+docker inspect v2tec/watchtower > /dev/null 2>&1
+
+if [[ $? != 0 ]]; then
+    text "Watchtower not installed"
+    text "Installing Watchtower"
+    docker pull v2tec/watchtower > /dev/null 2>&1 
+else
+    text "Watchtower installed"
+fi
