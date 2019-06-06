86547e8033feac0e2e8c6adb6dd506df85e4f16a
Keyshawn J. Butts <keyshawn_j_butts@homedepot.com>
2019-06-06 10:04:35 -0400

    --wip-- [skip ci]

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@
 # Variables
 # ###########################################################
 OSNAME=$(uname -s)
+echo "${OSNAME}"
 
 if [[ ${OSNAME} = "Darwin" ]]; then
-    curl -fsSL raw.githubusercontent.com/kjbappsllc/coffer/master/src/macos | cat
+    curl -fsSL raw.githubusercontent.com/kjbappsllc/coffer/master/src/macos/install.sh | cat
 else
     echo "Not supporting Windows OS at this time"
 fi
