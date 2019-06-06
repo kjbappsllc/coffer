3de127c4337d4875287b0508660108014ff5e43b
Keyshawn J. Butts <keyshawn_j_butts@homedepot.com>
2019-06-06 10:20:49 -0400

    --wip-- [skip ci]

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@
 OSNAME=$(uname -s)
 
 if [[ ${OSNAME} = "Darwin" ]]; then
-    echo "Setting up Coffer Mac"
-    ruby -e $(curl -fsSL raw.githubusercontent.com/kjbappsllc/coffer/master/src/macos/install.sh)
+    echo "Setting up Coffer"
+    sh -c $(curl -fsSL raw.githubusercontent.com/kjbappsllc/coffer/master/src/macos/install.sh)
 else
     echo "Not supporting Windows OS at this time"
 fi
